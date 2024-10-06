7d0105efb2dd57e870168547985f76d5d7fe49e7
Эдуард Гусев <f1ful1337@gmail.com>
2024-10-06 23:15:11 +0500

    Задача сделана.

--- Program.cs
+++ Program.cs
@@ -10,14 +10,12 @@ namespace Task2._5
     {
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("Введите длинны сторон прямоугольного параллелепипеда");
+        
+            Console.WriteLine("Введите длинны сторон прямоугольного параллелепипеда:");
              
          double A = Double.Parse(Console.ReadLine());
          double B = Double.Parse(Console.ReadLine());
          double C = Double.Parse(Console.ReadLine());
-
             Console.WriteLine("");
             double volume = A * B * C;
             double SurfaceArea = 2*(A * B + B * C + C * A);
